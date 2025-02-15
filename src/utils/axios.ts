c5717c50ac9085ec0de288e121d12da39e106e87
rianabd01 <rianwahyuab@gmail.com>
2025-02-15 23:54:03 +0700

    update

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL:
-    "https://ganapatih-be-rian-icukuofn7-rianabd01s-projects.vercel.app/",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
